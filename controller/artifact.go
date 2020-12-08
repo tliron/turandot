fe1d2b3ef628f82cf4adf3304b25a1e792977b9f
Tal Liron <tal.liron@gmail.com>
2020-12-08 11:57:24 -0600

    Move more code to Reposure

--- controller/artifact.go
+++ controller/artifact.go
@@ -15,11 +15,21 @@ func (self *Controller) publishArtifactsToRegistry(artifacts parser.KubernetesAr
 		for _, artifact := range artifacts {
 			if registry, err := self.Client.Reposure.RegistryClient().Get(service.Namespace, artifact.Registry); err == nil {
 				if err := self.Client.Reposure.RegistryClient().UpdateURLContext(registry, urlContext); err == nil {
-					// Note: OpenShift registry permissions require the namespace as the tag category
-					artifactName := fmt.Sprintf("%s/%s", service.Namespace, artifact.Name)
+					// Note: OpenShift registry permissions require the namespace as the repository
+					imageName := fmt.Sprintf("%s/%s", service.Namespace, artifact.Name)
 
-					if name, err := self.PublishOnRegistry(artifactName, artifact.SourcePath, registry, urlContext); err == nil {
-						artifactMappings[artifact.SourcePath] = name
+					if directClient, err := self.Client.Reposure.DirectClient(registry); err == nil {
+						if url, err := urlpkg.NewURL(artifact.SourcePath, urlContext); err == nil {
+							self.Log.Infof("publishing image %q at %q on %q", imageName, url.String(), directClient.Host)
+							if name, err := directClient.PushGzippedTarballFromURL(url, imageName); err == nil {
+								self.Log.Infof("published image %q at %q on %q", imageName, url.String(), directClient.Host)
+								artifactMappings[artifact.SourcePath] = name
+							} else {
+								return nil, err
+							}
+						} else {
+							return nil, err
+						}
 					} else {
 						return nil, err
 					}
--- go.mod
+++ go.mod
@@ -8,7 +8,6 @@ replace github.com/tliron/puccini => /Depot/Projects/RedHat/puccini
 
 require (
 	github.com/gofrs/flock v0.8.0
-	github.com/google/go-containerregistry v0.2.1
 	github.com/google/uuid v1.1.2
 	github.com/heptiolabs/healthcheck v0.0.0-20180807145615-6ff867650f40
 	github.com/jetstack/cert-manager v1.1.0
@@ -18,7 +17,7 @@ require (
 	github.com/tebeka/atexit v0.3.0
 	github.com/tliron/kutil v0.1.8
 	github.com/tliron/puccini v0.0.0-00010101000000-000000000000
-	github.com/tliron/reposure v0.1.0
+	github.com/tliron/reposure v0.1.1
 	github.com/tliron/yamlkeys v1.3.5
 	golang.org/x/crypto v0.0.0-20201203163018-be400aefbc4c
 	k8s.io/api v0.19.4
