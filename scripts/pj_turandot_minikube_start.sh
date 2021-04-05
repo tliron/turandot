ad40e3e9f6bb9f3b4944a9852ae7fae36fc4f71b
Paul Jordan <paul.m.jordan@bt.com>
2021-04-05 20:21:44 +0100

    remove pj_ files and references

--- scripts/pj_turandot_minikube_start.sh
+++
@@ -1,9 +0,0 @@
-minikube start --addons=registry ...
-kubectl create namespace workspace
-kubectl config set-context --current --namespace=workspace
-turandot operator install --site=central --role=view --wait -v
-reposure registry create default --provider=minikube --wait -v
-minikube status
-
-
-
